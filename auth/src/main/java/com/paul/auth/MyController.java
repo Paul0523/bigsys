a9dffa95b48fc5e5718972c5d5d0d7397a5efe75
fangzhipeng <750411463@qq.com>
2017-08-15 00:51:09 +0800

    完成初始化dubbo配置

--- auth/src/main/java/com/paul/auth/MyController.java
+++ auth/src/main/java/com/paul/auth/MyController.java
@@ -14,16 +14,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class MyController {
-	
-	@Autowired
-	private TestService testService;
-	
-	@RequestMapping(value = "/test")
-	@ResponseBody
-	public String testSay(@RequestParam(value = "name",defaultValue = "") String name){
-		StringBuffer sb = new StringBuffer();
-		sb.append("Dubbo: ").append(testService.sayHello(name));
-		return sb.toString();
-	}
-	
+
+    @Autowired
+    private TestService testService;
+
+    @RequestMapping(value = "/test")
+    @ResponseBody
+    public String testSay(@RequestParam(value = "name",defaultValue = "") String name){
+        StringBuffer sb = new StringBuffer();
+        sb.append("Dubbo: ").append(testService.sayHello(name));
+        return sb.toString();
+    }
+
 }
